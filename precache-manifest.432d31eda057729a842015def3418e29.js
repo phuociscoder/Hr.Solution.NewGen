652f0523c71545b4eb5f5d6a2a9e082752df3923
Phuoc Nguyen <huuphuoc.1109@gmail.com>
2021-07-20 12:30:00 +0700

    Updates

---
+++ precache-manifest.432d31eda057729a842015def3418e29.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9cb584f03229cf90bbc72bc9cfa542a",
+    "url": "/Hr.Solution.NewGen/index.html"
+  },
+  {
+    "revision": "f2e7a22998f16eab63e1",
+    "url": "/Hr.Solution.NewGen/static/css/2.87a9c7a2.chunk.css"
+  },
+  {
+    "revision": "bc7d8f427c1a3a8e9be8",
+    "url": "/Hr.Solution.NewGen/static/css/main.04489659.chunk.css"
+  },
+  {
+    "revision": "f2e7a22998f16eab63e1",
+    "url": "/Hr.Solution.NewGen/static/js/2.2b74fb6e.chunk.js"
+  },
+  {
+    "revision": "72d02e7f6ee5cf7f2c092b5489cefe22",
+    "url": "/Hr.Solution.NewGen/static/js/2.2b74fb6e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc7d8f427c1a3a8e9be8",
+    "url": "/Hr.Solution.NewGen/static/js/main.06e5668d.chunk.js"
+  },
+  {
+    "revision": "b60c6da29ada3a37d682",
+    "url": "/Hr.Solution.NewGen/static/js/runtime-main.5cc362fc.js"
+  },
+  {
+    "revision": "6f1e1c3c923193d183a91905f784706a",
+    "url": "/Hr.Solution.NewGen/static/media/avatar.6f1e1c3c.png"
+  },
+  {
+    "revision": "6345c7f11c639f9fcfc965f2105266f1",
+    "url": "/Hr.Solution.NewGen/static/media/logo.6345c7f1.png"
+  },
+  {
+    "revision": "dc6c8d20df3b475cebc1737d48c2eab8",
+    "url": "/Hr.Solution.NewGen/static/media/no-avatar.dc6c8d20.jpg"
+  }
+]);
