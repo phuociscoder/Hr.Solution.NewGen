9492bd4d0875a1da6cf5e97e249b20738166990f
Phuoc Nguyen <huuphuoc.1109@gmail.com>
2021-07-27 18:57:26 +0700

    update for backend structure

---
+++ Hr.Solution/Controllers/TestConnectController.cs
@@ -0,0 +1,28 @@
+﻿using Dapper;
+using Hr.Solution.Core;
+using Hr.Solution.Domain.Responses;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace Hr.Solution.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestConnectController : ControllerBase
+    {
+        private readonly IDbContext _dbContext;
+        public TestConnectController(IDbContext dbContext) {
+            _dbContext = dbContext;
+        }
+
+        [HttpGet, Route("")]
+        public ActionResult GetAll()
+        {
+            using (var connection = _dbContext.GetDBConnection())
+            {
+                var result = connection.Query<LsNationResponse>("SELECT * FROM LsNation").ToList();
+                return Ok(result);
+            }
+        }
+    }
+}
