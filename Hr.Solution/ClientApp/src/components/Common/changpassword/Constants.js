45bcf4b1be63f3c2f383e3e6ef0cba93ea163586
AI NHI <Nhi>
2021-08-31 18:24:45 +0700

    update message

--- Hr.Solution/ClientApp/src/components/Common/changpassword/Constants.js
+++ Hr.Solution/ClientApp/src/components/Common/changpassword/Constants.js
@@ -1,6 +1,6 @@
 export class ErrorCase{
-    static oldPassMatchNewPass = "Mật khẩu mới không được trùng mật khẩu cũ";
-    static confirmNotMatchNewPass = "Mật khẩu xác nhận không trùng khớp";
+    static oldPassMatchNewPass = "Mật khẩu mới không được trùng mật khẩu cũ!";
+    static confirmNotMatchNewPass = "Mật khẩu xác nhận không trùng khớp!";
     static fieldNull = "Vui lòng điền đầy đủ các trường!";
     static 
 }
